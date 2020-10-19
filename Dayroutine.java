7b4f6a1a50e0ab6d8e4969040ccb2e2113748bbc
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-19 21:44:53 +0530

    Maha(IV)Dayroutine

---
+++ Dayroutine.java
@@ -0,0 +1,22 @@
+class Routine{
+public static void main(String[]zip){
+	System.out.println("Hobbies");
+	main();
+}
+	public static void main(){
+		System.out.println("Music");
+		hungry();
+	}
+		public static void hungry(){
+			System.out.println("Drinking");
+				}
+	}
+	
+	
+	
+	
+	
+	
+
+   
+
